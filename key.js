a45fb57b3d9571ccaf749820ab8eb574ed551602
lokeshkagita <162789214+lokeshkagita@users.noreply.github.com>
2024-05-06 01:56:48 +0530

    Add files via upload

---
+++ key.js
@@ -0,0 +1,5 @@
+module.exports={
+    MONGOURI:"mongodb+srv://chakrinya:chakrinya009@nodeprojects.giz5b0e.mongodb.net/Tracking?retryWrites=true&w=majority",
+    SECRETKEY:"iojfojlasdjfoijwefkl;jasdkl;mnfnweojfajsdfl;weuiojhfoasjf[ijwejf",
+    PASS_GMAIL:"kycrozhwuumudiiy",
+}
